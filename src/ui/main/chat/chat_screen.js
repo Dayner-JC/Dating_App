610fe8d46440d15d861518d3af5dbcede6fc98f4
daynerjc <daynercespedes4@gmail.com>
2025-03-07 00:29:52 -0500

    feat(UI): Add UI support for block users

--- src/ui/main/chat/chat_screen.js
+++ src/ui/main/chat/chat_screen.js
@@ -7,12 +7,16 @@ import {
   Text,
   TextInput,
   View,
+  Modal,
 } from 'react-native';
 import ChatAppBar from '../appBars/chat_appBar';
 import IconButton from '../../components/icon_button';
 import PlayIcon from '../../../assets/icons/play.svg';
 import CameraIcon from '../../../assets/icons/camera.svg';
 import MicrophoneIcon from '../../../assets/icons/microphone.svg';
+import Button from '../../components/button';
+import CloseIcon from '../../../assets/icons/close.svg';
+import SlashIcon from '../../../assets/icons/slash.svg';
 import {
   Waveform,
   IWaveformRef,
@@ -266,6 +270,8 @@ const Message = ({message}) => {
 };
 
 const ChatScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.content}>
       <ChatAppBar />
@@ -282,6 +288,50 @@ const ChatScreen = () => {
           />
         )}
       </View>
+
+      <Modal
+        transparent={true}
+        animationType="fade"
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.modalContainer}>
+          <View style={styles.dialogBox}>
+            <View style={styles.dialogBar}>
+              <CloseIcon onPress={() => setModalVisible(false)} />
+            </View>
+            <SlashIcon/>
+            <Text style={styles.modal_title}>Block User</Text>
+            <Text style={styles.message}>
+            Are you sure you want to block this user?{'\n'}
+            This user will no longer be able to message you or view your profile.
+            </Text>
+            <Button
+              title={'Confirm Block'}
+              fontFamily="Roboto_500"
+              backgroundColor="#C3313C"
+              fontSize={14}
+              height={48}
+              width={302}
+              borderRadius={100}
+              onPress={() => {}}
+            />
+            <Button
+              title={'No, cancel'}
+              fontFamily="Roboto_400"
+              backgroundColor="#17261E"
+              borderWidth={1}
+              borderColor="#747474"
+              marginTop={12}
+              fontSize={14}
+              height={48}
+              width={302}
+              borderRadius={100}
+              onPress={() => setModalVisible(false)}
+            />
+          </View>
+        </View>
+      </Modal>
+
       <View style={styles.footer}>
         <TextInput
           placeholder="Write message"
@@ -359,6 +409,43 @@ const styles = StyleSheet.create({
   button: {
     height: 40,
     width: 40,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0A0F0DE5',
+  },
+  dialogBox: {
+    width: 350,
+    height: 372,
+    backgroundColor: '#17261E',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#21362C',
+  },
+  dialogBar: {
+    height: 36,
+    alignItems: 'flex-end',
+    backgroundColor: '#17261F',
+    width: '100%',
+    paddingStart: 10,
+  },
+  modal_title: {
+    fontFamily: 'Roboto_500',
+    fontSize: 22,
+    color: '#FFFFFF',
+    marginBottom: 10,
+    marginTop: 5,
+  },
+  message: {
+    fontFamily: 'Roboto_400',
+    fontSize: 14,
+    color: '#FFFFFF',
+    marginBottom: 20,
+    textAlign: 'center',
   },
 });
 
